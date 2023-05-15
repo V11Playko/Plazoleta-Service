79fc2daba4575528dbde78f2290bd17df5663149
Heinner Vega <heinnervega20@gmail.com>
2023-05-15 10:10:51 -0500

    HU2 Add test Jpa and rename data

--- src/test/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/adapter/RestaurantJpaAdapterTest.java
+++ src/test/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/adapter/RestaurantJpaAdapterTest.java
@@ -1,12 +1,26 @@
 package com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.adapter;
 
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.RestaurantEntity;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.mappers.IRestaurantEntityMapper;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IRestaurantRepository;
+import com.pragma.powerup.usermicroservice.domain.model.RestaurantModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class RestaurantJpaAdapterTest {
+    @InjectMocks
+    RestaurantJpaAdapter restaurantJpaAdapter;
+    @Mock
+    IRestaurantRepository restaurantRepository;
+    @Mock
+    IRestaurantEntityMapper restaurantMapper;
 
     @BeforeEach
     void setUp() {
@@ -15,5 +29,13 @@ class RestaurantJpaAdapterTest {
 
     @Test
     void saveRestaurant() {
+        RestaurantModel restaurant = MySqlData.obtainRestaurant();
+        RestaurantEntity restaurantEntity = MySqlData.obtainRestaurantEntity();
+
+        when(restaurantMapper.toEntityRestaurant(restaurant)).thenReturn(restaurantEntity);
+        restaurantJpaAdapter.saveRestaurant(restaurant);
+
+        verify(restaurantRepository).save(restaurantEntity);
+
     }
 }
