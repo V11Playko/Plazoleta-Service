8c1d2c008f289c3cfc51d671696684d72a46f47c
Heinner Vega <heinnervega20@gmail.com>
2023-05-15 08:21:59 -0500

    HU3 Add BEANS for dish and order

--- src/main/java/com/pragma/powerup/usermicroservice/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/powerup/usermicroservice/configuration/BeanConfiguration.java
@@ -1,11 +1,23 @@
 package com.pragma.powerup.usermicroservice.configuration;
 
 import com.pragma.powerup.usermicroservice.adapters.driven.client.UserClient;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.adapter.DishJpaAdapter;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.adapter.OrderJpaAdapter;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.adapter.RestaurantJpaAdapter;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.mappers.IDishEntityMapper;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.mappers.IOrderEntityMapper;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.mappers.IRestaurantEntityMapper;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IDishRepository;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IOrderRepository;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IRestaurantRepository;
+import com.pragma.powerup.usermicroservice.domain.api.IDishServicePort;
+import com.pragma.powerup.usermicroservice.domain.api.IOrderServicePort;
 import com.pragma.powerup.usermicroservice.domain.api.IRestaurantServicePort;
+import com.pragma.powerup.usermicroservice.domain.ports.IDishPersistencePort;
+import com.pragma.powerup.usermicroservice.domain.ports.IOrderPersistencePort;
 import com.pragma.powerup.usermicroservice.domain.ports.IRestaurantPersistencePort;
+import com.pragma.powerup.usermicroservice.domain.usecase.DishUseCase;
+import com.pragma.powerup.usermicroservice.domain.usecase.OrderUseCase;
 import com.pragma.powerup.usermicroservice.domain.usecase.RestaurantUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -25,4 +37,22 @@ public class BeanConfiguration {
         return new RestaurantUseCase(restaurantPersistencePort, userClient);
     }
 
+    // Order
+    @Bean
+    public IOrderPersistencePort orderPersistencePort(IOrderRepository orderRepository, IOrderEntityMapper orderEntityMapper){
+        return new OrderJpaAdapter(orderRepository, orderEntityMapper);
+    }
+    @Bean
+    public IOrderServicePort orderServicePort(IOrderPersistencePort orderPersistencePort) {
+        return new OrderUseCase(orderPersistencePort);
+    }
+    // Dish
+    @Bean
+    public IDishPersistencePort dishPersistencePort(IDishRepository dishRepository, IDishEntityMapper dishEntityMapper) {
+        return new DishJpaAdapter(dishRepository, dishEntityMapper);
+    }
+    @Bean
+    public IDishServicePort dishServicePort(IDishPersistencePort dishPersistencePort) {
+        return new DishUseCase(dishPersistencePort);
+    }
 }
