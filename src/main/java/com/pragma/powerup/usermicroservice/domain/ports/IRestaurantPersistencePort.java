bbcd4c22fc8f5971b101995ca10d006952186481
Heinner Vega <heinnervega20@gmail.com>
2023-05-23 13:58:23 -0500

    HU9 Changes These files were slightly modified to complete the listRestaurant method

--- src/main/java/com/pragma/powerup/usermicroservice/domain/ports/IRestaurantPersistencePort.java
+++ src/main/java/com/pragma/powerup/usermicroservice/domain/ports/IRestaurantPersistencePort.java
@@ -3,7 +3,10 @@ package com.pragma.powerup.usermicroservice.domain.ports;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.RestaurantEntity;
 import com.pragma.powerup.usermicroservice.domain.model.RestaurantModel;
 
+import java.util.List;
+
 public interface IRestaurantPersistencePort {
     RestaurantEntity saveRestaurant(RestaurantModel restaurantModel);
     RestaurantModel getRestaurant(Long id);
+    List<RestaurantModel> listByPageAndElements(int page, int numbersOfElements);
 }
