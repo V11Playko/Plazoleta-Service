01e83712b5f0f38648856646e77a7a414f9a6b9a
Heinner Vega <heinnervega20@gmail.com>
2023-06-08 08:12:43 -0500

    HU15 Add new Exception: SecurityCodeIncorrectException

---
+++ src/main/java/com/pragma/powerup/usermicroservice/domain/exceptions/SecurityCodeIncorrectException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.usermicroservice.domain.exceptions;
+
+public class SecurityCodeIncorrectException extends RuntimeException{
+    public SecurityCodeIncorrectException() {
+        super();
+    }
+}
