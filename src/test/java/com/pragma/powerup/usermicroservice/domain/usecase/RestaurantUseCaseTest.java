669d9139434a6b89cdf1401fae780f1b78cfe151
Heinner Vega <heinnervega20@gmail.com>
2023-06-27 11:16:45 -0500

    HU21 Add tests for deleteRestaurant method

--- src/test/java/com/pragma/powerup/usermicroservice/domain/usecase/RestaurantUseCaseTest.java
+++ src/test/java/com/pragma/powerup/usermicroservice/domain/usecase/RestaurantUseCaseTest.java
@@ -2,8 +2,13 @@ package com.pragma.powerup.usermicroservice.domain.usecase;
 
 import com.pragma.powerup.usermicroservice.adapters.driven.client.UserClient;
 import com.pragma.powerup.usermicroservice.adapters.driven.client.dtos.User;
+import com.pragma.powerup.usermicroservice.domain.exceptions.RestaurantHaveOrdersPending;
+import com.pragma.powerup.usermicroservice.domain.exceptions.RestaurantPendingDeleteException;
+import com.pragma.powerup.usermicroservice.domain.model.DishModel;
 import com.pragma.powerup.usermicroservice.domain.model.RestaurantModel;
 import com.pragma.powerup.usermicroservice.domain.ports.IDishPersistencePort;
+import com.pragma.powerup.usermicroservice.domain.ports.IOrderPersistencePort;
+import com.pragma.powerup.usermicroservice.domain.ports.IRestaurantEmployeePersistencePort;
 import com.pragma.powerup.usermicroservice.domain.ports.IRestaurantPersistencePort;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +19,8 @@ import org.mockito.MockitoAnnotations;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class RestaurantUseCaseTest {
@@ -24,13 +31,18 @@ class RestaurantUseCaseTest {
     private UserClient userClient;
     @Mock
     IDishPersistencePort dishPersistencePort;
+    @Mock
+    IOrderPersistencePort orderPersistencePort;
+    @Mock
+    IRestaurantEmployeePersistencePort restaurantEmployeePersistencePort;
 
     private RestaurantUseCase restaurantUseCase;
+
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        restaurantUseCase = new RestaurantUseCase(restaurantPersistencePort, userClient);
+        restaurantUseCase = new RestaurantUseCase(restaurantPersistencePort, dishPersistencePort, orderPersistencePort, restaurantEmployeePersistencePort, userClient);
     }
 
     @Test
@@ -56,5 +68,66 @@ class RestaurantUseCaseTest {
                 .thenReturn(Collections.singletonList(restaurantModel));
 
         Assertions.assertInstanceOf(List.class, restaurantUseCase.listRestaurant(0, 2));
+    }
+
+    @Test
+    void testDeleteRestaurant_NoOrdersPending_Success() {
+        Long restaurantId = 1L;
+
+        // Mockear el restaurante sin órdenes pendientes
+        RestaurantModel restaurant = new RestaurantModel();
+        restaurant.setId(restaurantId);
+        restaurant.setState("ACTIVE");
+
+        when(restaurantPersistencePort.getRestaurant(restaurantId)).thenReturn(restaurant);
+        when(restaurantPersistencePort.getNumberOfOrdersWithStateInPending(restaurantId)).thenReturn(0);
+
+        // Mockear las llamadas a los métodos relacionados
+        List<DishModel> emptyDishes = Collections.emptyList();
+        when(dishPersistencePort.getDishesByRestaurantId(restaurantId)).thenReturn(emptyDishes);
+        when(orderPersistencePort.getOrdersByRestaurantId(restaurantId)).thenReturn(Collections.emptyList());
+        when(restaurantEmployeePersistencePort.getEmployeesByRestaurantId(restaurantId)).thenReturn(Collections.emptyList());
+
+        // Ejecutar el método a probar
+        assertDoesNotThrow(() -> restaurantUseCase.deleteRestaurant(restaurantId));
+
+        // Verificar que se llamaron a los métodos relacionados
+        verify(restaurantPersistencePort).deleteRestaurantById(restaurantId);
+        for (DishModel dish : emptyDishes) {
+            verify(dishPersistencePort).deleteDishById(dish.getId());
+        }
+        verify(orderPersistencePort, times(0)).deleteOrderById(anyLong());
+        verify(restaurantEmployeePersistencePort, times(0)).deleteEmployeeByEmailAndRestaurantId(anyString(), anyLong());
+    }
+
+    @Test
+    void testDeleteRestaurant_OrdersPending_ExceptionThrown() {
+        Long restaurantId = 1L;
+
+        // Mockear el restaurante con órdenes pendientes
+        RestaurantModel restaurant = new RestaurantModel();
+        restaurant.setId(restaurantId);
+        restaurant.setState("ACTIVE");
+
+        when(restaurantPersistencePort.getRestaurant(restaurantId)).thenReturn(restaurant);
+        when(restaurantPersistencePort.getNumberOfOrdersWithStateInPending(restaurantId)).thenReturn(1);
+
+        // Ejecutar el método a probar y verificar que se lance la excepción esperada
+        assertThrows(RestaurantHaveOrdersPending.class, () -> restaurantUseCase.deleteRestaurant(restaurantId));
+    }
+
+    @Test
+    void testDeleteRestaurant_PendingDeleteState_ExceptionThrown() {
+        Long restaurantId = 1L;
+
+        // Mockear el restaurante en estado PENDING_DELETE
+        RestaurantModel restaurant = new RestaurantModel();
+        restaurant.setId(restaurantId);
+        restaurant.setState("PENDING_DELETE");
+
+        when(restaurantPersistencePort.getRestaurant(restaurantId)).thenReturn(restaurant);
+
+        // Ejecutar el método a probar y verificar que se lance la excepción esperada
+        assertThrows(RestaurantPendingDeleteException.class, () -> restaurantUseCase.deleteRestaurant(restaurantId));
     }
 }
